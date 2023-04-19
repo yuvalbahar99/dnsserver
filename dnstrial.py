a91f38ba35a42681d08c8500a8dca7495c018324
yuval <yuval.bahar99@gmail.com>
2023-04-19 16:21:01 +0300

    updated working for now

--- dnstrial.py
+++ dnstrial.py
@@ -2,9 +2,11 @@ import socket
 from scapy.all import *
 from scapy.layers.dns import DNS, DNSQR
 from scapy.layers.inet import *
+import logging
 
 
-DNS_IP = '10.0.0.138'
+DNS_IP = '172.16.255.254'
+queue_reqs = []
 
 
 def fil(packet):
@@ -13,14 +15,32 @@ def fil(packet):
     # and / packet[DNSQR].qname == 'ynet.co.il.'.encode()
 
 
+def add_to_queue(packets):
+    queue_reqs.append(packets[0])
+    logging.debug(str(packets[DNSQR].qname) + ' - into requests queue')
+
+
+def remove_from_queue():
+    packet = queue_reqs.pop(0)
+    logging.debug(str(packet[DNSQR].qname) + ' - out of requests queue')
+    return packet
+
+
 def main():
-    packets = sniff(count=1, lfilter=fil)
-    ip_client = packets[0][IP].src
-    my_dns = IP(dst=DNS_IP)/UDP(dport=53, sport=packets[0][UDP].sport)/packets[0][DNS]
-    response = sr1(my_dns)
-    res_client = IP(dst=ip_client)/UDP(sport=53, dport=packets[0][UDP].sport)/response[DNS]
-    send(res_client)
+    while True:
+        sniff(count=1, lfilter=fil, prn=add_to_queue)
+        logging.debug('sniffed packet')
+        current_packet = remove_from_queue()
+        ip_client = current_packet[IP].src
+        my_dns = IP(dst=DNS_IP)/UDP(dport=53, sport=current_packet[UDP].sport)/current_packet[DNS]
+        logging.debug(str(current_packet[DNSQR].qname) + ' - waiting for response of the ip')
+        response = sr1(my_dns)
+        logging.debug(str(current_packet[DNSQR].qname) + ' - received answer(ip address)')
+        res_client = IP(dst=ip_client)/UDP(sport=53, dport=current_packet[UDP].sport)/response[DNS]
+        send(res_client)
+        logging.debug(str(current_packet[DNSQR].qname) + ' - the ip was sent to the client')
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename="dnslog.log", level=logging.DEBUG)
     main()
